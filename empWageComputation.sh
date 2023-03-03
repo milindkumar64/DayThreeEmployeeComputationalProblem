fb77240a52e89f632d397736aa7b651cb380c8c3
milindkumar64 <milindgupta@gmail.com>
2023-03-04 00:08:28 +0530

    Refector: Refactor the Code to write a function to get work hours

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,17 +2,38 @@ echo "Welcome to Employee Wage Computation Program"
 
 isPartTime=1;
 isFullTime=2;
-empRatePerHr=20
+empHrs=0;
+totalSalary=0;
+totalWorkingHour=0;
+empRatePerHr=20;
+numWorkingDays=20;
+day=1;
 
-randomCheck=$((RANDOM%3));
-
-case $randomCheck in $isFullTime)
+function calculateWorkingHour() {
+	case $randomCheck in 
+                     $isFullTime)
                       empHrs=8;;
                      $isPartTime)
                       empHrs=4;;
                      *)
                       empHrs=0;;
 esac
+	echo $empHrs;
+}
 
-salary=$(($empHrs*$empRatePerHr))
-echo "salary=$salary"
+while [[ $day -le 20 && $totalWorkingHour -lt 100 ]]
+do
+randomCheck=$((RANDOM%3));
+
+	wHour=$(calculateWorkingHour $randomCheck);
+	totalWorkingHour=$(($totalWorkingHour + $wHour));
+	if [ $totalWorkingHour -gt 100 ]
+	then
+		totalWorkingHour=$(($totalWorkingHour - $wHour));
+		break;
+	fi
+	salary=$(($empRatePerHr*$wHour));
+	totalSalary=$(($totalSalary+$salary));
+	((day++));
+done
+echo "Employee has earned $totalSalary $ in a month (Total working Hour : $totalWorkingHour)";
